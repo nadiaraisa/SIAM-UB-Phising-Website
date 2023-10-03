62edfcc45fefad35c76b22a8772ad5ee803fff96
hilalfauzann <107779815+hilalfauzann@users.noreply.github.com>
2023-10-03 20:40:21 +0700

    Update success.blade.php

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -68,7 +68,7 @@ div.kc-logo-text{
     </div>
     <div class="card-pf border-0 rounded-0 shadow bg-body p-2 p-md-4">
         <header class="login-pf-header text-primary">
-                <h1 id="kc-page-title" class="h4"><strong>        Selamat kamu berhasil register 
+                <h1 id="kc-page-title" class="h4"><strong> Ups... Kamu kena pishing 
 </strong></h1>
       </header>
       <div id="kc-content">
